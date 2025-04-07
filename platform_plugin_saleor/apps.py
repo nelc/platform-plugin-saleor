5c64ba551ab05418acaf6c7e0c638483ff35f131
Andres Felipe Giraldo Malagon <andres.giraldo@edunext.co>
2025-04-07 17:04:57 -0500

    feat: initial project structure

---
+++ platform_plugin_saleor/apps.py
@@ -0,0 +1,13 @@
+"""
+platform_plugin_saleor Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class PlatformPluginSaleorConfig(AppConfig):
+    """
+    Configuration for the platform_plugin_saleor Django application.
+    """
+
+    name = 'platform_plugin_saleor'
