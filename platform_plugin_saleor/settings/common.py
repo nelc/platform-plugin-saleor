9a4ccb045aa42f1f00ece662d3690dafe8ed2276
Andres Felipe Giraldo Malagon <andres.giraldo@edunext.co>
2025-04-08 16:49:37 -0500

    refactor(settings): move settings to platform-plugin-saleor

--- platform_plugin_saleor/settings/common.py
+++ platform_plugin_saleor/settings/common.py
@@ -1,10 +1,9 @@
-"""
-Common Django settings for the Saleor project.
-"""
+"""Common Django settings for the Saleor project."""
 
-def plugin_settings(settings):
+
+def plugin_settings(settings):  # pylint: disable=unused-argument
+    """Update the common settings for the Saleor project."""
     # Update the provided settings module with any app-specific settings.
     # For example:
     #     settings.FEATURES['ENABLE_MY_APP'] = True
     #     settings.MY_APP_POLICY = 'foo'
-    pass
